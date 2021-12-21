8f7ada7198c4b447e6e7edfb122980f98383c435
logicielkr <graha@graha.kr>
2021-12-21 11:36:20 +0900

    1. 소스코드의 사소한 오류 수정    - ";" 누락 발견 수정

--- js/query.insert.js
+++ js/query.insert.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
 });
 function loadMemoHistoryById() {
 	var ext = location.pathname.substring(location.pathname.lastIndexOf(".") + 1);
-	var url = "autosaved.xml"
+	var url = "autosaved.xml";
 	if(queryId != null) {
 		url += "?query_id=" + queryId;
 	}
