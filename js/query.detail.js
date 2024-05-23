4aa780b2d65cae0b3742da5c4cea42676ba89da8
logicielkr <graha@graha.kr>
2024-05-23 20:10:22 +0900

    1. 버그 수정
    - 암호화된 게시물을 수정하는 화면에서 document.title 등도 복호화된 것을 표시해야 한다. #6

--- js/query.detail.js
+++ js/query.detail.js
@@ -9,8 +9,8 @@ function initDecrypt() {
 			var decrypted = PwdArea.decrypt(encrypted);
 			selectedTitle.text(decrypted);
 			selectedTitle.prepend("<i class='material-icons security'>security</i>");
-			$(document).attr("title", "메모(" + decrypted + ")");
-			$("h2.title").text("메모(" + decrypted + ")");
+			$(document).attr("title", "Query 실행기(수정)(" + decrypted + ")");
+			$("h2.title").text("Query 실행기(수정)(" + decrypted + ")");
 			encrypted = selectedContents.text();
 			selectedContents.text(PwdArea.decrypt(encrypted));
 		}
